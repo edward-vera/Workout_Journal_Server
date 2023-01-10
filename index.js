b79a6df72b4bd99f050842692e2e5195b0d110e5
edward-vera <103536879+edward-vera@users.noreply.github.com>
2023-01-09 22:53:09 -0600

    create get and post request for users resource

--- index.js
+++ index.js
@@ -6,15 +6,40 @@ const pool = require("./sql/connection")
 
 const PORT = process.env.PORT || 5000
 
+app.use(express.json())
+
 app.get('/', (req, res) => {
     res.json({message: "Hello Universe"})
 });
 
+//  GET list of users
 app.get('/users', (req, res) => {
-    pool.query("SELECT * FROM users", function(err, rows, fields) {
+    pool.query(`SELECT * FROM users`, function (err, rows, fields) {
         // Connection is automatically released when query resolves
     res.json(rows); 
     })
 });
+
+// GET User by id
+app.get('/users/:id', (req, res) => {
+    const {id} = req.params
+    pool.query(`SELECT * FROM users WHERE id = ${id}`, function (err, rows, fields) {
+        // Connection is automatically released when query resolves
+    res.json(rows)
+    })
+});
+
+// POST User by id
+app.post('/users', (req, res) => {
+    console.log(req.body);
+    pool.query(`INSERT INTO users (id, name, email, password) VALUES (?, ?, ?, ?)`, 
+    [null, req.body.name, req.body.email, req.body.password],
+    function (err, row, fields) {
+        // Connection is automatically released when query resolves
+    res.json(row);
+    })
+});
+
+
 
 app.listen(PORT, () => console.log(`Listening @ http://localhost:${PORT}`));
