34e58ef5fd7b1ac693bbe31c3feb28a597a27797
edward-vera <103536879+edward-vera@users.noreply.github.com>
2023-01-09 22:07:42 -0600

    get users added

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("hello");
+const express = require("express");
+
+const app = express();
+
+const pool = require("./sql/connection")
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+    res.json({message: "Hello Universe"})
+});
+
+app.get('/users', (req, res) => {
+    pool.query("SELECT * FROM users", function(err, rows, fields) {
+        // Connection is automatically released when query resolves
+    res.json(rows); 
+    })
+});
+
+app.listen(PORT, () => console.log(`Listening @ http://localhost:${PORT}`));
